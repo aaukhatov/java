5d5f93f6dcb2bb222cc6c98d1c7f5e78a9d3d310
Arthur Aukhatov <aaukhatov@gmail.com>
2020-05-15 23:43:49 +0300

    Modify a test to be platform independent way

--- src/test/java/art/aukhatov/http/WebClientTest.java
+++ src/test/java/art/aukhatov/http/WebClientTest.java
@@ -2,10 +2,12 @@ package art.aukhatov.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 class WebClientTest {
@@ -14,7 +16,8 @@ class WebClientTest {
 	void downloadByChunk() throws IOException, URISyntaxException, InterruptedException {
 		WebClient fd = new WebClient();
 		byte[] data = fd.download("http://www.africau.edu/images/default/sample.pdf", 1024);
-		final OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/sample3.pdf"));
+		Path path = Paths.get(System.getProperty("java.io.tmpdir") + File.separator + "sample.pdf");
+		OutputStream outputStream = Files.newOutputStream(path);
 		outputStream.write(data);
 		outputStream.flush();
 		outputStream.close();
