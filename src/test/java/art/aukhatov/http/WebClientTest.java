a0d9b00ab7f3d53c5c8837d0f4944fd968a86306
Arthur Aukhatov <aaukhatov@gmail.com>
2020-05-18 23:34:05 +0300

    Update test case

--- src/test/java/art/aukhatov/http/WebClientTest.java
+++ src/test/java/art/aukhatov/http/WebClientTest.java
@@ -17,14 +17,15 @@ class WebClientTest {
 	@Test
 	void downloadByChunk() throws IOException, URISyntaxException, InterruptedException {
 		WebClient fd = new WebClient();
-		byte[] data = fd.download("http://www.africau.edu/images/default/sample.pdf", 1024);
-		final String downloadedFilePath = System.getProperty("java.io.tmpdir") + File.separator + "sample.pdf";
+		byte[] data = fd.download("https://file-examples.com/wp-content/uploads/2017/10/file-example_PDF_1MB.pdf", 262_144);
+		final String downloadedFilePath = System.getProperty("java.io.tmpdir") + "sample.pdf";
+		System.out.println("File has downloaded to " + downloadedFilePath);
 		Path path = Paths.get(downloadedFilePath);
 		OutputStream outputStream = Files.newOutputStream(path);
 		outputStream.write(data);
 		outputStream.flush();
 		outputStream.close();
 
-		assertEquals(3028, Files.readAllBytes(Paths.get(downloadedFilePath)).length);
+		assertEquals(1_042_157, Files.readAllBytes(Paths.get(downloadedFilePath)).length);
 	}
 }
